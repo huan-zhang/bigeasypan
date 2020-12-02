ecb18f86fd45581eb4998636758b35e0d2937d46
hzhang <hzhang@192.168.0.157>
2020-12-02 08:37:39 -0800

    removed the home page carousel

--- index.php
+++ index.php
@@ -1,48 +1,6 @@
 <?php get_header(); ?>
 		<div class="row">
-			<div id="myCarousel" class="carousel slide" data-ride="carousel">
-	  			<!-- Indicators -->
-				<ol class="carousel-indicators" >
-				<?php //$counter = 0; if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-<?php for($i = 0; $i < 4; $i++ ) : ?>
-	<?php if ($i == 0) : ?>
-					<li data-target="#myCarousel" data-slide-to="<?php echo $i; ?>" class="active"></li>
-	<?php else : ?>
-					<li data-target="#myCarousel" data-slide-to="<?php echo $i; ?>" ></li>
-	<?php endif; ?>
-<?php endfor;?>
-				</ol>
-				<div class="carousel-inner" role="listbox">
-<?php for($i = 0; $i < 4; $i++ ) : ?>
-	<?php if ($i == 0) :  ?>
-					<div class="item active">
-	<?php else : ?>
-					<div class="item">
-	<?php endif; $post = get_carousel_post($i); setup_postdata($post); ?>
-						<img src="<?php echo getAttachUrl(get_the_ID()); ?>" class="current" alt="<?php echo get_the_Title();?>" />	
-						<div class="carousel-caption">
-							<h3><?php echo the_title(); ?></h3>
-							<p><?php echo the_content(" ", TRUE); ?></p> 
-							<?php $tmp = get_post_custom_values("forward_post", get_the_ID()); $forwardId = $tmp[0];?>
-							
-							<p><a href="<?php echo get_permalink($forwardId); ?>">更多...</a></p>
-						</div>
-					</div>	
-<?php endfor;?>
-				</div>		
-	  			
-				<!-- Wrapper for slides -->
-	
-	  			<!-- Left and right controls -->
-	  			<a class="left carousel-control" href="#myCarousel" role="button" data-slide="prev">
-	    			<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-	    			<span class="sr-only">Previous</span>
-	  			</a>
-	  			<a class="right carousel-control" href="#myCarousel" role="button" data-slide="next">
-	    			<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-	    			<span class="sr-only">Next</span>
-	  			</a>
-			</div>
+			<?php echo wp_get_attachment_image(HOMEPAGE_IMAGE_ID, array('600', '600'), "", array( "class" => "img-responsive center-block" ) );?>
 		</div>
 		<div class="row">
 <?php get_template_part( 'index-categories'); ?>	
