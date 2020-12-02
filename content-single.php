de81e38830f24c252870e517c37032312a6f53a7
hzhang <hzhang@192.168.0.157>
2020-12-02 12:54:53 -0800

    changed the site name on post content page

--- content-single.php
+++ content-single.php
@@ -2,7 +2,7 @@
 	<div class="col-xm-12">
 		<div id="breadcrumb">
 			<ol class="breadcrumb">
-				<li class="breadcrumb-item"><a href="<?php bloginfo( 'wpurl' );?>">大易盘</a></li>
+				<li class="breadcrumb-item"><a href="<?php bloginfo( 'wpurl' );?>"><?php echo bloginfo('name'); ?></a></li>
 				<li class="breadcrumb-item active"><a href="<?php echo get_category_link(get_current_cat()->cat_ID);?>"><?php echo get_current_cat()->name; ?></a></li> 
 				<!-- <li class="breadcrumb-item active"><?php $category->cat_name;?></li>-->
 			</ol>
