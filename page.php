0e23ec3150fbaf3ef023955ff2cc23be73f3ed7c
hzhang <hzhang@192.168.0.157>
2020-12-07 09:03:59 -0800

    modified page.php

--- page.php
+++ page.php
@@ -16,12 +16,6 @@ get_header(); ?>
 
 <div id="main-content" class="main-content">
 
-<?php
-if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
-	// Include the featured content template.
-	get_template_part( 'featured-content' );
-}
-?>
 	<div id="primary" class="content-area">
 		<div id="content" class="site-content" role="main">
 
@@ -42,5 +36,5 @@ if ( is_front_page() && twentyfourteen_has_featured_posts() ) {
 
 		</div><!-- #content -->
 	</div><!-- #primary -->
-	<?php get_sidebar( 'content' ); ?>
 </div><!-- #main-content -->
+<?php get_footer(); ?>
